25b3710ba7396ec9bd71e277d5b5ac15d531d179
Jonathan <jrwolf99@outlook.com>
2022-08-13 17:42:20 -0500

    changed to host remote

--- client/JavaScript/Socket/wsTransmit.js
+++ client/JavaScript/Socket/wsTransmit.js
@@ -1,5 +1,5 @@
 // const ws = new WebSocket("ws://localhost:3000");
-const ws = new WebSocket("ws://eclipse-server1.herokuapp.com/");
+const ws = new WebSocket("wss://eclipse-server1.herokuapp.com/");
 
 //websocket transmissions are conducted throughout the whole
 //client project. All modules handle their own transmissions.
