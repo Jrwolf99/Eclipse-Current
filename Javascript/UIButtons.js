267deca8ba9a9254c79b7485b9f22517c85bbc99
Jonathan <jrwolf99@outlook.com>
2022-08-18 21:51:15 -0500

    just the alert

--- Javascript/UIButtons.js
+++ Javascript/UIButtons.js
@@ -3,16 +3,3 @@ const showInfo = () => {
     "Welcome to Eclipse! This is a game designed by Bailey Garner and developed by Jonathan Wolf. It is currently in progress, but feel free to use the arrow keys to control the ship, press space to shoot, and turn the sound on! (This app is best supported in Chrome or Firefox.)"
   );
 };
-
-const toggleMusic = () => {
-  const myMusicButtonSlash = document.querySelector(".fa-slash");
-  if (BackgroundSong.duration > 0 && !BackgroundSong.paused) {
-    BackgroundSong.pause();
-    myMusicButtonSlash.style.color = "white";
-    return;
-  }
-  BackgroundSong.volume = 0.4;
-  BackgroundSong.play();
-
-  myMusicButtonSlash.style.color = "transparent";
-};
