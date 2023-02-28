07c887e58d253a057f98394741cef9c6ad1b222f
Jonathan Wolf <Jrwolf99@outlook.com>
2023-02-27 19:30:22 -0600

    changed websocket and also removed server

--- multiplayer/JavaScript/Socket/wsTransmit.js
+++ multiplayer/JavaScript/Socket/wsTransmit.js
@@ -1,5 +1,7 @@
 // const ws = new WebSocket("ws://localhost:3000");
-const ws = new WebSocket("wss://eclipse-server1.herokuapp.com/");
+const ws = new WebSocket(
+  "wss://eclipseserver-env.eba-mrnsysrh.us-east-1.elasticbeanstalk.com/"
+);
 
 //websocket transmissions are conducted throughout the whole
 //client project. All modules handle their own transmissions.
